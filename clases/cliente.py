941f0b9dd3e8763dd52367dca14532b627af80d9
alejomb257-hash <alejomb257@gmail.com>
2026-06-02 20:21:41 -0500

    Update cliente.py

---
+++ clases/cliente.py
@@ -0,0 +1,31 @@
+class Client:
+    
+    __name: str
+    __phone: str
+    __nroPets: int
+
+    def __init__(self, name, phone):
+        self.__name = name
+        self.__phone = phone
+        self.__pets = np.full([4], fill_value=None, dtype=Pet)
+        self.__nroPets = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def phone(self):
+        return self.__phone
+    
+    @property
+    def pets(self):
+        return self.__pets
+
+    @property
+    def nroPets(self):
+        return self.__nroPets
+
+    def agregarMascota(self, petName, species, age):
+        self.__pets[self.__nroPets] = Pet(petName, species, age)
+        self.__nroPets = self.__nroPets + 1
